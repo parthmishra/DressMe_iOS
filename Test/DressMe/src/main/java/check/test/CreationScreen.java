45433a587a72e345c8740a81cfd6191c455a7dff
dose5562 <dose5562@colorado.edu>
2015-12-07 03:15:17 -0700

    Created New Activity. Refactored button functionality

---
+++ Test/DressMe/src/main/java/check/test/CreationScreen.java
@@ -0,0 +1,71 @@
+package check.test;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class CreationScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creation_screen);
+
+        FloatingActionButton first = (FloatingActionButton) findViewById(R.id.first);
+        FloatingActionButton second = (FloatingActionButton) findViewById(R.id.second);
+        FloatingActionButton third = (FloatingActionButton) findViewById(R.id.third);
+        //FloatingActionButton fourth = (FloatingActionButton) findViewById(R.id.fourth);
+        //FloatingActionButton fifth = (FloatingActionButton) findViewById(R.id.fifth);
+
+        first.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+
+        second.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+
+        third.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+
+       /* fourth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+
+        fifth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+        */
+    }
+
+
+}
